774fee8ac579be3c72db3768db68851fa0441a2c
eirikmikal <eirikmikal@gmail.com>
2015-03-12 11:01:17 +0100

    Test

--- ShoppingCentre/src/Mallfunction/ShoppingCentre.java
+++ ShoppingCentre/src/Mallfunction/ShoppingCentre.java
@@ -5,11 +5,9 @@ package Mallfunction;
  * @author Erlend
  */
 public class ShoppingCentre {
+
     public static void main(String[] args) {
-       
-        
-        
         System.out.println("hei");
     }
-    
+
 }
