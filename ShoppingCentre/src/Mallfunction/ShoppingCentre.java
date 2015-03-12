cd1770a3c732eb9b0e3199d9c719b11a2738ce41
chwizard <chriwi@student.hist.no>
2015-03-12 10:47:07 +0100

    Har gjort enda en kommentarendring

--- ShoppingCentre/src/Mallfunction/ShoppingCentre.java
+++ ShoppingCentre/src/Mallfunction/ShoppingCentre.java
@@ -7,7 +7,7 @@ package Mallfunction;
 public class ShoppingCentre {
     public static void main(String[] args) {
         System.out.println("hei");
-        
        // dette er endring
+        // dette er enda en endring
     }
 }
